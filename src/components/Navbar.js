c5f066b60b6bdf414ca07f2d15bf80dcf4aa12c2
gulshkr <gulshansinghmuzz.591@gmail.com>
2021-08-30 17:41:21 +0530

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 export default function Navbar(props) {
   return (
@@ -8,9 +8,9 @@ export default function Navbar(props) {
       className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
     >
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
+        <a className="navbar-brand" href="#">
           {props.title}
-        </Link>
+        </a>
         <button
           className="navbar-toggler"
           type="button"
@@ -25,15 +25,16 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">
+              {/* <Link className="nav-link active" aria-current="page" to="/">
                 Home
-              </Link>
+              </Link> */}
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
             </li>
-            <li className="nav-item">
+            {/* <li className="nav-item">
               <Link className="nav-link" to="/about">
                 {props.aboutText}
               </Link>
-            </li>
+            </li> */}
           </ul>
           <form className="d-flex">
             {/* <input
